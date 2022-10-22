d6a9aa2d0ba61aa178e475c386182cba705c8f32
roger <impressor7@gmail.com>
2022-10-23 02:35:10 +0900

    Fix style bugs related to Button

--- src/components/Button.css.tsx
+++ src/components/Button.css.tsx
@@ -1,41 +1,41 @@
 import { colorVars, vars } from "src/styles/theme.css";
 
 import { recipe, RecipeVariants } from "@vanilla-extract/recipes";
+import { Typography } from "src/styles/recipe.css";
 
 export const button = recipe({
-  base: {
-    height: 40,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingLeft: 16,
-    paddingRight: 16,
+  base: [
+    Typography({ variant: "bodysm", bold: true }),
+    {
+      height: 40,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      paddingLeft: 16,
+      paddingRight: 16,
 
-    background: colorVars.semantics.primary,
-    borderRadius: vars.border.radius[8],
-    border: "none",
+      background: colorVars.semantics.primary,
+      borderRadius: vars.border.radius[8],
+      border: "none",
 
-    fontWeight: "bold",
-    fontSize: 14,
-    lineHeight: 20,
-    letterSpacing: "0.01em",
+      color: colorVars.palette.white,
 
-    color: colorVars.palette.white,
-
-    selectors: {
-      "&:hover": {
-        opacity: 0.8,
+      selectors: {
+        "&:hover": {
+          opacity: 0.8,
+        },
       },
     },
-  },
+  ],
 
   variants: {
     size: {
-      lg: {
-        height: 48,
-        fontSize: 16,
-        lineHeight: 24,
-      },
+      lg: [
+        Typography({ variant: "bodymd", bold: true }),
+        {
+          height: 48,
+        },
+      ],
     },
   },
 });
