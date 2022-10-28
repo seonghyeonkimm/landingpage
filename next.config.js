22c3d14e2d3f756b721e73550150baaf2e3e22bb
roger <impressor7@gmail.com>
2022-10-28 22:46:33 +0900

    Update package.json

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
